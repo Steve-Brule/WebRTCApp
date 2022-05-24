eacc37e7c50335c21443189c01f2e250f93c5a75
Steve-Brule <pqd@tuta.io>
2022-05-24 01:48:00 +0100

    Initial Commit

---
+++ WebRTClient/WebRTClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebRTClientApp.swift
+//  WebRTClient
+//
+//  Created by nick on 24.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WebRTClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
